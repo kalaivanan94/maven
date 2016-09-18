71aee4cc258f3ed6f7120839dfd91540c52ed436
kalaivanan94 <s.kalaivanan007@gmail.com>
2016-09-18 19:28:47 +0530

    category name

---
+++ 10th upload/PetWorld/src/main/java/com/petworld/model/Product.java
@@ -0,0 +1,54 @@
+package com.petworld.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int pid;
+    private String name;
+    private String description;
+    private double price;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategoryname() {
+		return categoryname;
+	}
+	public void setCategory(String categoryname) {
+		this.categoryname = categoryname;
+	}
+	public String getSupplier() {
+		return supplier;
+	}
+	public void setSupplier(String supplier) {
+		this.supplier = supplier;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+    
+}
+
