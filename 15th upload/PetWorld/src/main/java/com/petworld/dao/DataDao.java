1dcf02ae6f051a742d8f007356cc34357566a769
kalaivanan94 <s.kalaivanan007@gmail.com>
2016-09-27 19:16:15 +0530

    15th upload

---
+++ 15th upload/PetWorld/src/main/java/com/petworld/dao/DataDao.java
@@ -0,0 +1,18 @@
+package com.petworld.dao;
+
+import java.util.List;
+
+import com.petworld.model.Product;
+
+public interface DataDao 
+{
+	public int insertRow(Product product,String s);
+
+	public List getList();
+
+	public Product getRowById(int pid);
+
+	public int updateRow(Product employee);
+
+	public int deleteRow(int pid);
+}
